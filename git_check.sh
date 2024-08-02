a8efbbe0b31e6085c71d5552719a6ba9b529260f
liliang9693 <liliang_wk@163.com>
2024-08-02 10:22:04 +0800

    test

--- git_check.sh
+++ git_check.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
-# Change to the directory of your git repository
 echo "Script started: $(date)" >> ~/check.log
 # Check for changes on the remote repository
 if git fetch origin && [ `git rev-list HEAD...origin/master --count` != 0 ] ; then
     echo "+++++++" >> ~/check.log
     # If changes exist, pull the changes
-    git pull
+    git pull  >> ~/check.log
     echo "Script build: $(date)" >> ~/check.log
     # Build the project using mkdocs
-    mkdocs build
+    mkdocs build  >> ~/check.log
     echo "Script build end: $(date)" >> ~/check.log
     # Restart the nginx service
-    sudo systemctl restart nginx
+    sudo systemctl restart nginx  >> ~/check.log
     echo "restart end: $(date)" >> ~/check.log
 fi
 echo "----------" >> ~/check.log
